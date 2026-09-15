24ca9d0b0ca1ca61e1f9b8b1129b9fdf77bc788d
osamasado <osama.sadow@gmail.com>
2026-09-15 10:51:00 +0200

    Add RawMaterial entity
    Internal/back-of-house only - never shown to guests, so unlike
    Category/Meal it has no translations.

---
+++ backend/src/main/java/org/restaurantordersmanagement/backend/menu/RawMaterialRepository.java
@@ -0,0 +1,6 @@
+package org.restaurantordersmanagement.backend.menu;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RawMaterialRepository extends JpaRepository<RawMaterial, Long> {
+}
